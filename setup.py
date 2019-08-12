f4d8b76cb8fc38f0c33af6cb86c0c723f26e702b
Vykstorm <victorruizgomezdev@gmail.com>
2019-08-12 15:57:00 +0200

    Created setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+
+from distutils.core import setup, Extension
+import numpy as np
+
+
+ext = Extension('numpy_bool_argmax_ext', sources = ['argmax.c'], include_dirs=[np.get_include()])
+
+setup(
+    name = 'numpy-bool-argmax-ext',
+    version = '1.0.0',
+    description = 'Additional helper methods to improve numpy.argmax performance for non-contiguous 1D boolean arrays',
+    ext_modules = [ext])
