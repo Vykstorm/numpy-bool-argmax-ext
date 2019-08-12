84212ace71bfd0e4583d6daa7ea67061f2298fe6
Vykstorm <victorruizgomezdev@gmail.com>
2019-08-12 20:00:44 +0200

    Explictily set the language of the extenstion to 'c'

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ from distutils.core import setup, Extension
 import numpy as np
 
 
-ext = Extension('numpy_bool_argmax_ext', sources = ['argmax.c'], include_dirs=[np.get_include()])
+ext = Extension('numpy_bool_argmax_ext',
+    sources = ['argmax.c'],
+    include_dirs=[np.get_include()],
+    language='c',
+    )
 
 setup(
     name = 'numpy-bool-argmax-ext',
