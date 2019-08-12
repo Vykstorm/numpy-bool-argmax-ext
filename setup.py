c1ea95310fdbf63ce44eff17daab3634acd7a4e9
Vykstorm <victorruizgomezdev@gmail.com>
2019-08-12 20:31:55 +0200

    Added package dir info

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ import numpy as np
 
 
 ext = Extension('numpy_bool_argmax_ext',
-    sources = ['argmax.c'],
+    sources =['argmax.c'],
     include_dirs=[np.get_include()],
     language='c',
     )
 
 setup(
-    name = 'numpy-bool-argmax-ext',
-    version = '1.0.0',
-    description = 'Additional helper methods to improve numpy.argmax performance for non-contiguous 1D boolean arrays',
-    ext_modules = [ext])
+    name='numpy-bool-argmax-ext',
+    version='1.0.0',
+    description='Additional helper methods to improve numpy.argmax performance for non-contiguous 1D boolean arrays',
+    ext_modules=[ext],
+    install_requires=['numpy'],
+    package_dir={'argmaxext': 'argmaxext'})
