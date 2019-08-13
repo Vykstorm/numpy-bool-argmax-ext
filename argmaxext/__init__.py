612f88369b7c5146596ac474a185f81227066701
Vykstorm <victorruizgomezdev@gmail.com>
2019-08-13 18:09:19 +0200

    Reimplemented function

--- argmaxext/__init__.py
+++ argmaxext/__init__.py
@@ -1,7 +1,8 @@
 
 
 import numpy as np
-from numpy_bool_argmax_ext import reversed_bool_argmax
+from numpy_bool_argmax_ext import bool_argmax
+from functools import partial
 
 __all__ = ['argmax']
 
@@ -9,11 +10,19 @@ __all__ = ['argmax']
 def argmax(x, axis=None, out=None):
     '''
     This function is equivalent to numpy.argmax, but it is optimized
-    when you pass a 1D boolean array with strides (-1,) and the rest of the arguments
+    when you pass a 1D boolean array with strides and the rest of the arguments
     (axis and out) are set to None.
     '''
     x = np.asarray(x)
+    default_argmax = partial(np.argmax, x, axis, out)
 
-    if x.dtype == np.bool and (x.ndim == 0 or (x.ndim == 1 and x.strides == (-1,))) and x.base is not None:
-        return len(x)-reversed_bool_argmax(x.base)-1
-    return np.argmax(x, axis, out)
+    if x.dtype != np.bool or x.ndim != 1 or (x.flags.aligned and x.flags.c_contiguous) or x.base is None:
+        return default_argmax()
+
+    base, stride = x.base, x.strides[0]
+    if (base.size-1)//stride+1 != x.size:
+        return default_argmax()
+
+    if stride > 0:
+        return bool_argmax(base, stride)
+    return len(base)-bool_argmax(base, stride)-1
